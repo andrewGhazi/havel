71348586442f465499435941ecf3315b70102511
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2025-10-11 15:23:21 -0400

    precomputed package info for testing

--- data-raw/pak_results.R
+++ data-raw/pak_results.R
@@ -5,4 +5,4 @@ ut_res = havel:::get_info_tools("utils", dep_type = c("depends", "imports", "lin
 
 pak_results = list(dt_res, gg_res, ut_res)
 
-usethis::use_data(pak_results, overwrite = TRUE)
+usethis::use_data(pak_results, overwrite = TRUE, internal = TRUE)
