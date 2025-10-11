fac5a345a9392705dbf11c5232145fa18fe215e0
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2025-10-11 13:51:11 -0400

    data-raw

---
+++ data-raw/pak_results.R
@@ -0,0 +1,8 @@
+
+dt_res = pak::pkg_deps("data.table")
+gg_res = pak::pkg_deps("ggplot2")
+ut_res = havel:::get_info_tools("utils", dep_type = c("depends", "imports", "linkingto"))
+
+pak_results = list(dt_res, gg_res, ut_res)
+
+usethis::use_data(pak_results, overwrite = TRUE)
