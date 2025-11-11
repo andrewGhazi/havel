92c2ef26cc4f56bfc145a81e8c8a1c0cba67fceb
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2025-11-10 22:10:42 -0500

    expose example data, document

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Example results from \code{\link[pak:pkg_deps]{pkg_deps}}
+#'
+#' This is a named list containing pre-computed examples of package dependency
+#' information for packages \code{data.table}, \code{ggplot2}, and \code{utils}
+#' pulled on Monday November 10th 2025. The first two use
+#' \code{\link[pak:pkg_deps]{pkg_deps}} from the \code{pak} package and the last
+#' one uses an internal function in \code{havel} that falls back to
+#' \code{\link[tools:package_dependencies]{package_dependencies}} from
+#' \code{tools} (\code{\link[pak:pkg_deps]{pkg_deps}} fails on base packages).
+#' These precomputed results are used to provide data to run the examples
+#' without requiring internet access (notably on CRAN's servers).
+#' @format A named list containing three data frames of package dependency
+#'   information:
+#' \describe{
+#'   \item{data.table}{a tibble describing \code{data.table}'s dependencies}
+#'   \item{ggplot2}{a tibble describing \code{ggplot2}'s dependencies}
+#'   \item{utils}{a data.table describing \code{utils}'s dependencies}
+#' }
+"pkg_deps_ex"
--- data-raw/pak_results.R
+++ data-raw/pak_results.R
@@ -3,6 +3,8 @@ dt_res = pak::pkg_deps("data.table")
 gg_res = pak::pkg_deps("ggplot2")
 ut_res = havel:::get_info_tools("utils", dep_type = c("depends", "imports", "linkingto"))
 
-pak_results = list(dt_res, gg_res, ut_res)
+pkg_deps_ex = list("data.table" = dt_res,
+                   "ggplot2" = gg_res,
+                   "utils" = ut_res)
 
-usethis::use_data(pak_results, overwrite = TRUE, internal = TRUE)
+usethis::use_data(pkg_deps_ex, overwrite = TRUE)
